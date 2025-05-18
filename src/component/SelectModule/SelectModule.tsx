0bd0f9ffae4df9aebb8644e381d100bf08330088
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-05-18 23:02:40 +0300

    refactor: style for Wrapper component

--- src/component/SelectModule/SelectModule.tsx
+++ src/component/SelectModule/SelectModule.tsx
@@ -1,17 +1,31 @@
 import { CustomSelect } from "custom-component-ui-kelzerock";
 import { Wrapper } from "../Wrapper/Wrapper";
-import "./SelectModule.css";
 import { OptionsData } from "custom-component-ui-kelzerock/build/models/interfaces";
 
 const slugOptions: OptionsData[] = [
-  { value: 1, title: "one", id: 1 },
-  { value: 2, title: "two", id: 2 },
-  { value: 3, title: "three", id: 3 },
+  { value: 1, title: "one" },
+  { value: 2, title: "two" },
+  { value: 3, title: "three" },
 ];
 export const SelectModule = () => {
   return (
     <Wrapper title="Select">
       <CustomSelect title="title" options={slugOptions} />
+      <CustomSelect title="title" required options={slugOptions} />
+      <CustomSelect title="title" required disabled options={slugOptions} />
+      <CustomSelect
+        title="title"
+        required
+        disabled
+        options={[
+          ...slugOptions,
+          {
+            value: 4,
+            title: "four",
+            defaultSelect: true,
+          },
+        ]}
+      />
     </Wrapper>
   );
 };
